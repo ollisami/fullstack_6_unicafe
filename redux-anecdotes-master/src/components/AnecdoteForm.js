78d443dbecee5285a0d03fdaac777e3cf9b6908d
Ollila <sami-pekka.ollila@cgi.com>
2019-02-26 13:59:47 +0200

    tehtävät 6.8

---
+++ redux-anecdotes-master/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {createAnecdote} from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = ({store}) => {
+
+    const create = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        store.dispatch(
+            createAnecdote(content)
+        )
+        event.target.anecdote.value = ''
+    }
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={create}>
+                <div><input name='anecdote'/></div>
+                <button type='submit'>create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
